79be0f51725560af7fb146aae720e5fb09215122
jose <josegbluz@protonmail.com>
2024-07-06 14:45:01 -0300

    Improved layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,18 @@
-import { Route } from '@angular/router';
-import { DairyActivityPage } from './ui/pages/dairy-activity/dairy-activity.page';
+import {Route} from '@angular/router';
+import {DairyActivityPage} from './ui/pages/dairy-activity/dairy-activity.page';
 
 export interface AppRoute extends Route {
-    path?: RoutePath;
-    children?: Array<AppRoute>;
+  path?: RoutePath;
+  children?: Array<AppRoute>;
 }
 
-export enum RoutePath{
-    DairyActivity = ''
+export enum RoutePath {
+  DairyActivity = ''
 }
 
 export const routes: Array<AppRoute> = [
-    {
-        path: RoutePath.DairyActivity,
-        component: DairyActivityPage
-    }
+  {
+    path: RoutePath.DairyActivity,
+    component: DairyActivityPage
+  }
 ];
