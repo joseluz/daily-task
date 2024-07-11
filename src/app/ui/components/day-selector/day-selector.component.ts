ff3d4f2f34193fdd062866b0ff634480678b401d
jose <josegbluz@protonmail.com>
2024-07-11 20:51:56 -0300

    Applied button component

--- src/app/ui/components/day-selector/day-selector.component.ts
+++ src/app/ui/components/day-selector/day-selector.component.ts
@@ -1,19 +1,21 @@
 import { DatePipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { addDays } from 'date-fns';
+import {ButtonComponent} from "../button/button.component";
 
 @Component({
   selector: 'dt-day-selector',
   standalone: true,
-  imports: [
-    DatePipe
-  ],
+    imports: [
+        DatePipe,
+        ButtonComponent
+    ],
   templateUrl: './day-selector.component.html'
 })
 export class DaySelectorComponent {
   @Input() currentDate: Date = new Date();
   @Output() currentDateChange = new EventEmitter<Date>();
-  
+
   previousDay(): void{
     this.currentDate = addDays(this.currentDate, -1);
     this.currentDateChange.emit(this.currentDate);
